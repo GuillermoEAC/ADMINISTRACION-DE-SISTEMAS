58d5db8ea3123ce0091f2b00c3bc41594f9bbd11
Kamila Garcia <big.western.beacon@gmail.com>
2026-03-19 10:51:33 -0700

    Update tarea7_ssl.sh

--- Linux/tarea7_ssl.sh
+++ Linux/tarea7_ssl.sh
@@ -226,7 +226,7 @@ realizar_resumen_instalacion() {
     # Parche de paciencia: Java es pesado y tarda en despertar
     if [ "$serv" == "Tomcat" ]; then
         echo -e "${AMARILLO}[*] Dando 4 segundos para que Java inicie motores...${RESET}"
-        sleep 4
+        sleep 10
     fi
 
     echo -e "\n${AZUL}=========================================${RESET}"
